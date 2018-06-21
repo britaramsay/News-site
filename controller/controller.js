a7954f05593aaa0077b8ee7ef1034e777759ebe8
Brita Ramsay <britaramsay@gmail.com>
2018-06-21 15:30:07 -0400

    Added correct photo link and bootstrap

--- controller/controller.js
+++ controller/controller.js
@@ -2,77 +2,78 @@ const   express = require('express'),
         router = express.Router(),
         request = require("request"),
         cheerio = require("cheerio"),
-
         db = require("../models");
 
-router.get('/', function (req, res) {
+router.get('/headlines', function (req, res) {
+    var added = 0;
     request('https://movieweb.com/new-movies/', function (err, response, html) {
         var $ = cheerio.load(html)
 
         $('.movie').each((index, element) => {
-            if(element.children[0].children[0])
-                var photo = element.children[0].children[0].attribs.href
             var title = element.children[1].children[0].children[0].children[0].data
-            var link = element.children[1].children[0].children[0].attribs.href
-            if(element.children[1].children[0].next.children[1].children[0].attribs.class == 'movie-genre')
-                var genre = element.children[1].children[0].next.children[1].children[0].children[0].data
-            var synopsis = element.children[1].children[0].next.next.children[0].children[0].data
+            var photo = $(element).children().text().substring($(element).children().text().indexOf('https'), $(element).children().text().indexOf('" alt'))
+            console.log('\n')
+           
+            // Look for this movie in the database
             db.Movie.findOne({ title: title }).then(function (data) {
+
+                // Create new movie if it is not found
                 if(data == null) {
-                    db.Movie.create({ title: title, link: link, genre: genre, synopsis: synopsis, photo: photo })
+                    // Save photo and genre if movie has one                    
+                    // if(element.children[0].children[0])
+                    if(element.children[1].children[0].next.children[1].children[0].attribs.class == 'movie-genre')
+                        var genre = element.children[1].children[0].next.children[1].children[0].children[0].data
+        
+                    db.Movie.create({ 
+                        title: title, 
+                        link: element.children[1].children[0].children[0].attribs.href, 
+                        genre: genre, 
+                        synopsis: element.children[1].children[0].next.next.children[0].children[0].data, 
+                        photo: photo 
+                    })
                     .then(function(dbMovie) {
-                        console.log(dbMovie);
+                        added++
+                        console.log(added)
+                        
+                        return res.json(added)
                     })
                     .catch(function(err) {
-                        // return res.json(err);
+                        return res.json(err);
                     });
                 }
             })
         })
     })
-    res.render('index')    
+    // res.render('index', added)    
 })
 
-router.get('/headlines', (req, res) => {
+router.get('/', (req, res) => {
     db.Movie.find({})
     .populate('comment')
     .then(function (data) { 
         data.map((element) => {
             element.numComments = element.comment.length
         })
-        res.render('partials/headlines', {headlines: data, layout:false})    
+        res.render('index', { headlines: data })    
     })    
 })
 
 router.post('/addComment/:id', (req, res) => {
-    console.log(req.body)
+
     db.Comment.create(req.body).then(function (comment) {  
-    console.log('udhkj')
-        
         return db.Movie.findOneAndUpdate({ _id: req.params.id }, { $push: { comment: comment._id } }, { new: true })
-            .then(function(dbMovie) {
-            // If the User was updated successfully, send it back to the client
+        .then(function(dbMovie) {
+        // If the User was updated successfully, send it back to the client
             res.json(dbMovie);
-            })
-            .catch(function(err) {
-            // If an error occurs, send it back to the client
+        })
+        .catch(function(err) {
+        // If an error occurs, send it back to the client
             res.json(err);
         });
     })
 })
 
-router.get("/articles/:id", function(req, res) {
-    db.Movie.findById(req.params.id) 
-        .populate('comment')    
-        .then(function (movie) {  
-            res.json(movie)
-        })   
-        .catch(function(err) {
-        // If an error occurs, send it back to the client
-        res.json(err);
-    });
-});
-
+// Get all comments on the movie
 router.get('/getComments/:id', (req, res) => {
     db.Movie.findOne({_id: req.params.id}).then(function (movie) {
         var comments = []
--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,26 @@
+// Get headlines clicked
 $("#headlines").on("click", function (){
     $.get('/headlines').then(function(data) {
+        // Empty and append headlines
+        console.log(data)
         $("#data").empty()
         $('#data').append(data)
     })
 })
 
+// Leave comment clicked
 $(document).on('click', ".comment", function () {  
+    console.log('heda')
+    
+    // Show comment form
     $("#form-"+$(this).data("id")).show()
-    console.log($(this).data("id"))
 })
 
+// View all comments on a movie clicked
 $(document).on('click', ".getComments", function () {  
-    // console.log($(this).attr('id'))
+    // Get comments on this movie
     $.get('/getComments/'+$(this).attr('id')).then((data) => {  
-        console.log($(this).attr('id'))
+        // Append comments
         $('#comments-'+$(this).attr('id')).append(data)
     })
-
 })
